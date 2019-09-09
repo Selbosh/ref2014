32682b4f60797c1b62a1a25fc1fbf9e10673d8d7
Selbosh <david@selbys.org.uk>
2019-09-09 10:40:30 +0100

    Increment version

---
+++ R/ref2014.R
@@ -0,0 +1,12 @@
+#' REF 2014 package
+#'
+#' Access to data from the Research Excellence Framework 2014
+#'
+#' See \url{https://github.com/Selbosh/ref2014} for more info.
+#'
+#' @docType package
+#' @name ref2014
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
