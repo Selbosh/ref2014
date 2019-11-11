458aa3a815fd0bafeba8e3885f711eea31be0a65
Selbosh <david@selbys.org.uk>
2019-11-11 14:05:20 +0000

    Add Chemistry DOIs to article_metadata

--- data-raw/download.R
+++ data-raw/download.R
@@ -17,20 +17,22 @@ dois <- subset(tidy_outputs(),
                & uoa_name %in% c('Economics and Econometrics',
                                  'Mathematical Sciences',
                                  'Biological Sciences',
-                                 'Physics')
+                                 'Physics',
+                                 'Chemistry')
                & !is.na(doi))$doi
 dois <- unique(tolower(dois))
 already_downloaded <- readr::read_csv('article_metadata.csv')
 dois <- dois[!dois %in% tolower(already_downloaded$doi)]
 
 dois_list <- split(dois, ceiling(seq_along(dois) / 100))
-article_metadata <- rcrossref::cr_works(dois_list[[1]], .progress = 'text')$data %>%
-  select(container.title, doi, issn, subject, title, publisher)
-readr::write_csv(article_metadata, 'article_metadata.csv')
+# article_metadata2 <- rcrossref::cr_works(dois_list[[1]], .progress = 'text')$data %>%
+  # select(container.title, doi, issn, subject, title, publisher)
+# readr::write_csv(article_metadata2, 'article_metadata2.csv')
+library(dplyr)
 num_left <- length(dois_list) - 1
-for (chunk in tail(dois_list, 48)) {
+for (chunk in dois_list) {
   Sys.sleep(3)
-  message(num_left, ' to go')
+  message(num_left, ' chunks to go')
   article_metadata <- rcrossref::cr_works(chunk, .progress = 'text')$data %>%
     select(container.title, doi, issn, subject, title, publisher)
   readr::write_csv(article_metadata, 'article_metadata.csv', append = TRUE)
@@ -53,4 +55,4 @@ article_metadata <- article_metadata %>%
   left_join(group_by(., journal_id) %>%
               summarise(unique_journal_title = first(container.title)),
             by = 'journal_id')
-usethis::use_data(article_metadata)
+usethis::use_data(article_metadata, overwrite = TRUE)
